816010da56a164f6a11251f060bdcb025d1bde19
dineshgetty <82610495+dineshgetty@users.noreply.github.com>
2026-04-23 18:47:57 +0530

    Add data loading module

--- src/data.py
+++ src/data.py
@@ -1,1 +1,7 @@
+import yfinance as yf
+import numpy as np
 
+def download_data(stocks, start, end):
+    data = yf.download(stocks, start=start, end=end, interval="1d", auto_adjust=True)["Close"]
+    data = data.sort_index()
+    return np.log(data)
