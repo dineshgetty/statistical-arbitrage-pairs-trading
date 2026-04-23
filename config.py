132f1f54a9f6a00a309a893a77ab046eebd82ade
dineshgetty <82610495+dineshgetty@users.noreply.github.com>
2026-04-23 18:55:49 +0530

    Add configuration file

---
+++ config.py
@@ -0,0 +1,37 @@
+sector_stocks = {
+    "BANK NIFTY" : ["HDFCBANK.NS", "SBIN.NS", "ICICIBANK.NS", "AXISBANK.NS", 
+                    "KOTAKBANK.NS", "UNIONBANK.NS", "PNB.NS", "CANBK.NS", 
+                    "FEDERALBNK.NS", "AUBANK.NS", "INDUSINDBK.NS", "YESBANK.NS", 
+                    "IDFCFIRSTB.NS"],
+    
+    "FIN NIFTY" : ["HDFCBANK.NS", "SBIN.NS", "ICICIBANK.NS", "BAJFINANCE.NS", 
+                  "AXISBANK.NS", "KOTAKBANK.NS", "BAJAJFINSV.NS", "SBILIFE.NS", 
+                  "SHRIRAMFIN.NS", "JIOFIN.NS", "HDFCLIFE.NS", "MUTHOOTFIN.NS", 
+                  "PFC.NS", "CHOLAFIN.NS", "BSE.NS", "ICICIGI.NS", 
+                   "RECLTD.NS", "SBICARD.NS", "MFSL.NS", "LICHSGFIN.NS"],
+
+    "FMCG" : ["HINDUNILVR.NS", "ITC.NS", "NESTLEIND.NS", "VBL.NS", 
+              "BRITANNIA.NS", "GODREJCP.NS", "TATACONSUM.NS", "MARICO.NS", 
+              "UNITDSPR.NS", "DABUR.NS", "PATANJALI.NS", "COLPAL.NS", 
+              "UBL.NS", "RADICO.NS", "EMAMILTD.NS"],
+
+    "NIFTY AUTO" : ["MARUTI.NS", "M&M.NS", "BAJAJ-AUTO.NS", "EICHERMOT.NS", 
+                    "TVSMOTOR.NS", "MOTHERSON.NS", "TMPV.NS", "HEROMOTOCO.NS", 
+                    "BOSCHLTD.NS", "ASHOKLEY.NS", "BHARATFORG.NS", "UNOMINDA.NS", 
+                    "TIINDIA.NS", "SONACOMS.NS", "EXIDEIND.NS"],
+
+    "NIFTY IT" : ["TCS.NS", "INFY.NS", "HCLTECH.NS", "WIPRO.NS", 
+                  "TECHM.NS", "LTM.SN", "PERSISTENT.NS", "OFSS.NS", 
+                  "MPHASIS.NS", "COFORGE.NS"],
+
+    "NIFTY METAL" : ["JSWSTEEL.NS", "VEDL.NS", "TATASTEEL.NS", "ADANIENT.NS", 
+                     "HINDZINC.NS", "HINDALCO.NS", "JINDALSTEL.NS", "LLOYDSME.NS", 
+                     "NATIONALUM.NS", "NMDC.NS", "SAIL.NS", "JSL.NS", 
+                     "APLAPOLLO.NS", "HINDCOPPER.NS", "WELCORP.NS"],
+
+    "NIFTY PHARMA" : ["SUNPHARMA.NS", "DIVISLAB.NS", "TORNTPHARM.NS", "LUPIN.NS", 
+                      "DRREDDY.NS", "CIPLA.NS", "ZYDUSLIFE.NS", "MANKIND.NS", 
+                      "AUROPHARMA.NS", "ALKEM.NS", "GLENMARK.NS", "BIOCON.NS", 
+                      "ABBOTINDIA.NS", "LAURUSLABS.NS", "IPCALAB.NS", "AJANTPHARM.NS", 
+                      "JBCHEPHARM.NS", "GLAND.NS", "WOCKPHARMA.NS", "PPLPHARMA.NS"]
+}
